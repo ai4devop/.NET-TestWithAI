b9bbfba743a3b165897485d61369047444144586
Thomas Curti <t.curti@groupeonepoint.com>
2025-02-06 14:51:05 +0100

    Make it iso

--- exercices/Exercice3/Exercice3/Domain/User.cs
+++
@@ -1,2 +0,0 @@
-public record User(long id, string name, string phoneNumber, Role? role)
-{ }
--- exercices/Exercice3/Exercice3/Repository/UserRepository.cs
+++
@@ -1,9 +0,0 @@
-public interface IUserRepository {
-  User findById(long id);
-
-  User findByPhoneNumber(string phoneNumber);
-
-  void save(User user);
-
-  void deleteUser(long id);
-}
